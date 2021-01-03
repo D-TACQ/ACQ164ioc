01994d48a711598ca15e8ec8cd412a1fd20e42f7
Peter Milne <peter.milne@d-tacq.com>
2021-01-03 14:33:46 +0000

    testAsynPortDriver runs as part of ACQ164 ioc  On branch master 	modified:   acq164App/src/Makefile 	new file:   acq164App/src/testAsynPortDriver.cpp 	new file:   acq164App/src/testAsynPortDriver.h 	modified:   iocBoot/iocacq164/st.cmd

--- iocBoot/iocacq164/st.cmd
+++ iocBoot/iocacq164/st.cmd
@@ -11,6 +11,23 @@ cd "${TOP}"
 dbLoadDatabase "dbd/acq164.dbd"
 acq164_registerRecordDeviceDriver pdbbase
 
+# Turn on asynTraceFlow and asynTraceError for global trace, i.e. no connected asynUser.
+asynSetTraceMask("", 0, 17)
+
+testAsynPortDriverConfigure("testAPD", 1000, 8)
+
+dbLoadRecords("db/testAsynPortDriver.db","P=testAPD:,R=scope1:,PORT=testAPD,ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=1,ADDR=0,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=2,ADDR=1,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=3,ADDR=2,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=4,ADDR=3,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=5,ADDR=4,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=6,ADDR=5,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=7,ADDR=6,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynWaveform.db","P=testAPD:,R=scope1:,PORT=testAPD,CH=8,ADDR=7,TIMEOUT=1,NPOINTS=1000")
+dbLoadRecords("db/asynRecord.db","P=testAPD:,R=asyn1,PORT=testAPD,ADDR=0,OMAX=80,IMAX=80")
+
+
 ## Load record instances
 dbLoadTemplate "db/user.substitutions"
 dbLoadRecords "db/acq164Version.db", "user=pgm"
